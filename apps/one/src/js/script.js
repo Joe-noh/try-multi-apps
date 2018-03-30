2901f137548af32365f14f0ea0f104578f2899cf
Joe-noh <goflb.jh@gmail.com>
2018-03-31 00:55:20 +0900

    one uses common

--- apps/one/src/js/script.js
+++ apps/one/src/js/script.js
@@ -1,6 +1,11 @@
+import add from 'common';
+
 const app = document.querySelector('#app');
 
 const elem = document.createElement("span")
-elem.textContent = "This is App One."
+elem.textContent = `
+  This is App One.
+  ${add(1, 2)}
+`;
 
 app.appendChild(elem)
